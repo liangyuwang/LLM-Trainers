1369419edde5ce82081431d0f955de84c307b444
liangyuwang <105261065+liangyuwang@users.noreply.github.com>
2024-03-06 02:21:44 +0800

    Delete llmtrainers directory

--- llmtrainers/__init__.py
+++
@@ -1,9 +0,0 @@
-def trainer_cls(select='pytorch-ddp'):
-    if select == 'pytorch-base':
-        from llmtrainers.pytorch import BaseTrainer
-        return BaseTrainer
-    elif select == 'pytorch-ddp':
-        from llmtrainers.pytorch import DDPTrainer
-        return DDPTrainer
-    else:
-        raise ValueError(f"Unknown trainer type: {select}")
--- llmtrainers/pytorch/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import Trainer as BaseTrainer
-from .ddp import Trainer as DDPTrainer
-from .pipe import Trainer as PipeTrainer
-
-# from .training_args import TrainingArguments
